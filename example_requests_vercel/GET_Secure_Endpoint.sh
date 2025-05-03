f58e2237e2c57eeb4e230ab3706432338cbfbd35
Vitor Lucena <104996449+vitorlucena1@users.noreply.github.com>
2025-05-03 01:16:33 +0000

    Conserto dos dados

--- example_requests_vercel/GET_Secure_Endpoint.sh
+++ example_requests_vercel/GET_Secure_Endpoint.sh
@@ -1,3 +1,3 @@
 curl --request GET \
   --url 'https://backend-express-mongodb-two.vercel.app/protected' \
-  --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MTUwNmVhM2JmMzA5OWIzODA2NjE1ZiIsImlhdCI6MTc0NjIzMDU4OCwiZXhwIjoxNzQ2MjM0MTg4fQ.ody58q91xBHGOLm85CKZZN54lxGDaKJ86tt5fVNix-k'
+  --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MTUwNmVhM2JmMzA5OWIzODA2NjE1ZiIsImlhdCI6MTc0NjIzNDk1OSwiZXhwIjoxNzQ2MjM4NTU5fQ.5GVANTyl-ja5WtaAM_3Xywp91Lg-4ZBhq8ww18LtuHs'
