2e15077c5e1701bb59ae6cd001207f380766f460
Vitor Lucena <104996449+vitorlucena1@users.noreply.github.com>
2025-05-03 00:03:51 +0000

    Ajuste no teste do vercel

--- example_requests_vercel/GET_Secure_Endpoint.sh
+++ example_requests_vercel/GET_Secure_Endpoint.sh
@@ -1,3 +1,3 @@
 curl --request GET \
   --url 'https://backend-express-mongodb-two.vercel.app/protected' \
-  --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MTUwNmVhM2JmMzA5OWIzODA2NjE1ZiIsImlhdCI6MTc0NjIxMTgyMywiZXhwIjoxNzQ2MjE1NDIzfQ.ITkLf3Y2zG35Y5Akyd5NXmEmgg06baErkbdxoAzfl1E'
+  --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MTUwNmVhM2JmMzA5OWIzODA2NjE1ZiIsImlhdCI6MTc0NjIzMDU4OCwiZXhwIjoxNzQ2MjM0MTg4fQ.ody58q91xBHGOLm85CKZZN54lxGDaKJ86tt5fVNix-k'
