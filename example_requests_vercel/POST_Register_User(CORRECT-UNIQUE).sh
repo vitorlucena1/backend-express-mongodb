9ee1b3258b2df3965e5aade9ef0a9d67c4704761
Vitor Lucena <104996449+vitorlucena1@users.noreply.github.com>
2025-05-03 00:07:45 +0000

    Ajuste do teste para o vercel

--- example_requests_vercel/POST_Register_User(CORRECT-UNIQUE).sh
+++ example_requests_vercel/POST_Register_User(CORRECT-UNIQUE).sh
@@ -2,7 +2,7 @@ curl --request POST \
   --url 'https://backend-express-mongodb-two.vercel.app/users/register' \
   --header 'Content-Type: application/json' \
   --data '{
-    "username": "newuser",
-    "email":"newuser@example.com",
+    "username": "newuser2",
+    "email":"newuser@example2.com",
     "password": "securepassword123"
     }'
